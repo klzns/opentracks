7483377d694d1e5dd4bd3a6ec1c3c88a7167607f
Breno Calazans <brenocalazans@gmail.com>
2014-05-03 18:54:28 -0300

    Design account page

--- app/web/api.py
+++ app/web/api.py
@@ -30,6 +30,18 @@ def nym_page(nym):
     accounts = ot_account.accounts_for_nym(nym)['accounts']
     return render_template('nym/nym.html', accounts=accounts)
 
+@mod_web.route('/account/<string:accountId>', methods=['GET'])
+def account_page(accountId):
+    account = ot_account.get_account_info(accountId)['account']
+
+    inbox = ot_account.inbox(accountId)
+    if 'error' in inbox: return jsonify(inbox), 500
+
+    outbox = ot_account.outbox(accountId)
+    if 'error' in outbox: return jsonify(outbox), 500
+
+    return render_template('account/account.html', account=account, inbox=inbox, outbox=outbox)
+
 @mod_web.route('/stat')
 def stat():
     return jsonify(ot.stat())
